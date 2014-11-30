20b4ead7bdaa6b85ba572c073d30bf6d0e63579f
Adam Stanton <adamstanton1979@gmail.com>
2014-11-30 07:00:19 -0800

    Update half-interval function, add helpers.

---
+++ 1.3.1/ex134.scm
@@ -0,0 +1,23 @@
+;; Exercise 1.34
+
+(load "../helpers.scm")
+
+(define (f g)
+  (g 2))
+
+;; Returns 4
+(f sqr)
+
+;; Returns 8
+(f cube)
+
+;; Returns 3
+(f 1+)
+
+;; Returns an error (*** ERROR IN f, (console)@344.3 -- Operator is not a PROCEDURE)
+(f f)
+
+;; This can be explained by:
+;; -> (f f)
+;;   -> (g 2) (substituting g for 2)
+;;   -> (2 2) -> error rased here.
---
+++ helpers.scm
@@ -0,0 +1,16 @@
+;; Helper functions used in many of the exercises
+
+;; Increment number
+(define (1+ n) (+ n 1))
+
+;; Decrement number
+(define (1- n) (- n 1))
+
+;; Square number
+(define (sqr n) (expt n 2))
+
+;; Cube number
+(define (cube n) (* n (sqr n)))
+
+;; Return paramter (not required after lambda section)
+(define (identity n) n)
