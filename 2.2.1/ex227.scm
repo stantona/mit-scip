40e12c3f2f23de1de1e6fa0eafcd04759f67ce6f
Adam Stanton <adamstanton1979@gmail.com>
2015-01-28 09:57:06 -0800

    Add exercise 2.27 for the deep-reverse algorithm.

---
+++ 2.2.1/ex227.scm
@@ -0,0 +1,27 @@
+;; Exercise 2.27
+
+(define x (list (list 1 2) (list 3 4)))
+;; ((1 2) (3 4))
+
+;; The base problem is when the items is empty, in which 
+;; case we return the new list.
+;;
+;; The secondary problem is taking the car of the list and
+;; cons on to the new list. (This is the standard reverse).
+;;
+;; The tertiary problem is that elements that are lists must
+;; also be reversed.
+(define (deep-reverse items)
+  (define (traverse item)
+    (if (not (list? item))
+        item
+        (iter item '())))
+
+  (define (iter old new)
+    (if (null? old)
+        new
+        (iter (cdr old)
+              (cons (traverse (car old)) new)))))
+
+  (trace iter)
+  (iter items '()))
