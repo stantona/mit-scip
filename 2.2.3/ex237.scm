d70f1b79abf0d978fcdb0f14c6bdf7e8835e563a
Adam Stanton <adamstanton1979@gmail.com>
2015-03-08 19:25:57 -0700

    Complete exercise 2.37

---
+++ 2.2.3/ex237.scm
@@ -0,0 +1,22 @@
+;; Exercise 2.37 matrix algebra
+
+(load "../helpers.scm")
+;; Load the exercise for accumulate-n
+(load "ex236.scm")
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda(x)
+         (dot-product x v))
+       m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda(v)
+           (matrix-*-vector cols v))
+         m)))
