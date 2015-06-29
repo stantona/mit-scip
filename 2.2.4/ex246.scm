e8958a2dc5b2bdd6cededd7a5cf3412931b8ff25
Adam Stanton <adamstanton1979@gmail.com>
2015-06-28 18:47:01 -0700

    Add exercise 2.46

---
+++ 2.2.4/ex246.scm
@@ -0,0 +1,28 @@
+;; Exercise 2.46
+;;
+;; Functions to make a vector, add a vector and multiply.
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (add-vect vect1 vect2)
+  (make-vect
+   (+ (xcor-vect vect1) (xcor-vect vect2))
+   (+ (ycor-vect vect1) (ycor-vect vect2))))
+
+(define (sub-vect vect1 vect2)
+  (make-vect
+   (- (xcor-vect vect1) (xcor-vect vect2))
+   (- (ycor-vect vect1) (ycor-vect vect2))))
+
+(define (scale-vector vect scale)
+  (make-vect
+   (* (xcor-vect vect) scale)
+   (* (ycor-vect vect) scale)))
+
+  
