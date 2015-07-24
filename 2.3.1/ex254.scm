5aa48e18f23bd5643f89a1528bd82e85a4368aca
Adam Stanton <adamstanton1979@gmail.com>
2015-07-24 10:52:56 -0700

    Complete Exercise 2.54

---
+++ 2.3.1/ex254.scm
@@ -0,0 +1,15 @@
+;; Exercise 2.54
+;; Implementation of equal?. Equal? compares lists of atoms. The set must much exactly, including the order.
+
+(define (equal1? lat1 lat2)
+  (define (lists-empty?)
+    (and (null? lat1) (null? lat2)))
+
+  (define (either-list-empty?)
+    (or (null? lat1) (null? lat2)))
+
+  (cond ((lists-empty?) #t)
+        ((either-list-empty?) #f)
+        ((eq? (car lat1) (car lat2)) (equal? (cdr lat1) (cdr lat2)))
+        (else
+         #f)))
