c4b978c283118beccdb7a260c545333eaf035d5d
Adam Stanton <adamstanton1979@gmail.com>
2015-12-05 09:30:26 -0800

    Refactor lookup procedure with btree procedures
    Use the btree representation instead of raw scheme function to access
    each side of the branch.
    
    That is, instead of using "car" to get the currenty entry, use "entry"
    as defined in the btree.scm file.
    
    Resolves ex 2.66.

--- 2.3.3/ex266.scm
+++ 2.3.3/ex266.scm
@@ -1,10 +1,12 @@
+(load "btree.scm")
+
 (define (key record) record)
 
 (define (lookup given-key set-of-records)
   (cond ((null? set-of-records) false)
-        ((equal? given-key (key (car set-of-records)))
-         (car set-of-records))
+        ((equal? given-key (key (entry set-of-records)))
+         (entry set-of-records))
         ((< given-key (key (car set-of-records)))
-         (lookup given-key (cadr set-of-records)))
+         (lookup given-key (left-branch set-of-records)))
         ((> given-key (key (car set-of-records)))
-         (lookup given-key (caddr set-of-records)))))
+         (lookup given-key (right-branch set-of-records)))))
